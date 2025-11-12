8659c273f86c5ca8f2baebb88428914e795edc71
Riflusso <vadim2005110@gmail.com>
2025-11-12 15:53:22 +0400

    Fix onItemDraw and doSwap recursion

--- common/src/main/java/dev/shwg/smoothswapping/mixin/DrawContextMixin.java
+++ common/src/main/java/dev/shwg/smoothswapping/mixin/DrawContextMixin.java
@@ -43,20 +43,29 @@ public abstract class DrawContextMixin {
     @Final
     @Shadow
     private MinecraftClient client;
+
+    @Unique
+    private static boolean smooth_Swapping$isRendering = false;
+
     @Shadow
     public abstract void drawItem(ItemStack item, int x, int y);
     @Shadow public abstract void drawItemInSlot(TextRenderer textRenderer, ItemStack stack, int x, int y, @Nullable String countOverride);
 
     @Inject(method = "drawItem(Lnet/minecraft/entity/LivingEntity;Lnet/minecraft/world/World;Lnet/minecraft/item/ItemStack;IIII)V", at = @At("HEAD"), cancellable = true)
     public void onItemDraw(LivingEntity entity, World world, ItemStack stack, int x, int y, int seed, int z, CallbackInfo cbi) {
-        if (smooth_Swapping$isHotbar() && !(client.currentScreen instanceof ConfigScreen)) return;
-
-        if (((ItemStackAccessor) (Object) stack).smooth_Swapping$isSwapStack()) return;
+        if (smooth_Swapping$isRendering) return;
 
         try {
+            smooth_Swapping$isRendering = true;
+
+            if (smooth_Swapping$isHotbar() && !(client.currentScreen instanceof ConfigScreen)) return;
+            if (((ItemStackAccessor) (Object) stack).smooth_Swapping$isSwapStack()) return;
+
             smooth_Swapping$doSwap(stack, x, y, cbi);
         } catch (Exception e) {
             SwapUtil.reset();
+        } finally {
+            smooth_Swapping$isRendering = false;
         }
     }
 
